e6c3efa57ad7eb09b9deda5d00d128be465eaf19
Satish <satishaurange@sumagoinfotech.com>
2024-01-12 11:45:00 +0530

    changes for the order status update new updated

--- app/Http/Controllers/DistributorMobileAppController.php
+++ app/Http/Controllers/DistributorMobileAppController.php
@@ -605,7 +605,7 @@ class DistributorMobileAppController extends Controller
                             })
                             ->where(function ($query1) use ($request) {
                                 return $query1->where('tbl_order_summary.is_deleted','no')
-                                ->orWhere('tbl_order_summary.forwarded_bsc_id',$request->created_disctributor_id)
+                                ->where('tbl_order_summary.forwarded_bsc_id',$request->created_disctributor_id)
                                 ->orWhere('tbl_order_summary.forwarded_dsc_id',$request->created_disctributor_id);
                             })
                             ->select(
