a931b6c460fb3429d8b08829ebfc1e8545aedcb5
Satish <satishaurange@sumagoinfotech.com>
2024-01-24 19:12:38 +0530

    changes for the api promotion new arrival

--- app/Http/Controllers/WebAPIController.php
+++ app/Http/Controllers/WebAPIController.php
@@ -6635,6 +6635,7 @@ class WebAPIController extends Controller
                             'user_type'=>'dsc'
                         ]);
                         Dist_Promotion_Demotion::where('user_id_need_to_promote_demote',$request->user_id)->update(['is_updated'=>'y']);
+                        UsersInfo::where(['user_id'=>$request->user_id])->update(['new_list_to_view'=>'n']);
                     }
                 } elseif($dist_promotion->user_type_old=='fsc') {
 
@@ -6647,6 +6648,7 @@ class WebAPIController extends Controller
                             'user_type'=>'bsc'
                         ]);
                         Dist_Promotion_Demotion::where('user_id_need_to_promote_demote',$request->user_id)->update(['is_updated'=>'y']);
+                        UsersInfo::where(['user_id'=>$request->user_id])->update(['new_list_to_view'=>'n']);
                     }
                 }
             } 
