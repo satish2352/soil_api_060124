520a569efb30b0ce5bdce93b8278bba33b2e41c6
Satish <satishaurange@sumagoinfotech.com>
2024-01-29 11:42:43 +0530

    changes for target video view admin api

--- app/Http/Controllers/DistributorControllerNandu.php
+++ app/Http/Controllers/DistributorControllerNandu.php
@@ -2669,12 +2669,16 @@ class DistributorControllerNandu extends Controller
         {
 
             $targetvideo = VideoWatchHistory::
-                                    leftJoin('tbl_target_videos', function($join) {
-                                        $join->on('video_watch_history.video_id','=','tbl_target_videos.id');
+                                    leftJoin('tbl_target_videos_to_distributor', function($join) {
+                                        $join->on('video_watch_history.video_id','=','tbl_target_videos_to_distributor.id');
+                                    })
+                                    ->leftJoin('tbl_target_videos', function($join) {
+                                        $join->on('tbl_target_videos_to_distributor.target_vedio_id','=','tbl_target_videos.id');
                                     })
                                     ->leftJoin('usersinfo', function($join) {
                                         $join->on('video_watch_history.user_id','=','usersinfo.user_id');
                                     })
+                                    // ->where('video_watch_history.user_id',$request->dist_id)
                                     ->select(   'video_watch_history.*',
                                                 'usersinfo.fname',
                                                 'usersinfo.mname',
