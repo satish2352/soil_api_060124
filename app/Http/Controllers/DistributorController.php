6883772f7c1d07b7703a961907d02e7ceae2fa42
Satish <satishaurange@sumagoinfotech.com>
2024-02-02 11:42:57 +0530

    changes for image uplodad

--- app/Http/Controllers/DistributorController.php
+++ app/Http/Controllers/DistributorController.php
@@ -3344,8 +3344,8 @@ class DistributorController extends Controller
             {   
                 // $photo_two_lat_long=explode("_",$request->lat_long_string);
                 $photo_one_lat_long=explode("_",$request->lat_long_string);
-                $photo_two_lat = $photo_one_lat_long[2]; //2
-                $photo_two_long = $photo_one_lat_long[3]; //3
+                $photo_two_lat = $photo_one_lat_long[0]; //2
+                $photo_two_long = $photo_one_lat_long[1]; //3
                 $filename=$this->processUpload($request, $inputfilenametoupload,$imagedataPath,$photoName);
                 $sctresult=SCTResult::where('id',$idLastInserted)->update(['photo_two'=>$filename,'photo_two_lat'=>$photo_two_lat,'photo_two_long'=>$photo_two_long]);
             
@@ -3358,8 +3358,8 @@ class DistributorController extends Controller
             {     
                 // $photo_three_lat_long=explode("_",$request->lat_long_string);
                 $photo_one_lat_long=explode("_",$request->lat_long_string);
-                $photo_three_lat = $photo_one_lat_long[4];
-                $photo_three_long = $photo_one_lat_long[5];
+                $photo_three_lat = $photo_one_lat_long[0];
+                $photo_three_long = $photo_one_lat_long[1];
                 $filename=$this->processUpload($request, $inputfilenametoupload,$imagedataPath,$photoName);
                 $sctresult=SCTResult::where('id',$idLastInserted)->update(['photo_three'=>$filename,'photo_three_lat'=>$photo_three_lat,'photo_three_long'=>$photo_three_long]);
             
@@ -3372,8 +3372,8 @@ class DistributorController extends Controller
             {   
                 // $photo_four_lat_long=explode("_",$request->lat_long_string);
                 $photo_one_lat_long=explode("_",$request->lat_long_string);
-                $photo_four_lat = $photo_one_lat_long[6];
-                $photo_four_long = $photo_one_lat_long[7];
+                $photo_four_lat = $photo_one_lat_long[0];
+                $photo_four_long = $photo_one_lat_long[1];
                 $filename=$this->processUpload($request, $inputfilenametoupload,$imagedataPath,$photoName);
                 $sctresult=SCTResult::where('id',$idLastInserted)->update(['photo_four'=>$filename,'photo_four_lat'=>$photo_four_lat,'photo_four_long'=>$photo_four_long]);
             
@@ -3385,8 +3385,8 @@ class DistributorController extends Controller
             {     
                 // $photo_five_lat_long=explode("_",$request->lat_long_string);
                 $photo_one_lat_long=explode("_",$request->lat_long_string);
-                $photo_five_lat = $photo_one_lat_long[8];
-                $photo_five_long = $photo_one_lat_long[9];
+                $photo_five_lat = $photo_one_lat_long[0];
+                $photo_five_long = $photo_one_lat_long[1];
                 $filename=$this->processUpload($request, $inputfilenametoupload,$imagedataPath,$photoName);
                 $sctresult=SCTResult::where('id',$idLastInserted)->update(['photo_five'=>$filename,'photo_five_lat'=>$photo_five_lat,'photo_five_long'=>$photo_five_long]);
             
