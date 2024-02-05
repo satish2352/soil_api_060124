636b4289e822e1bfe3b5350b705d0d7249b16283
Satish <satishaurange@sumagoinfotech.com>
2024-02-05 15:08:47 +0530

    changes for web sct result api issues

--- app/Http/Controllers/WebAPIController.php
+++ app/Http/Controllers/WebAPIController.php
@@ -5923,10 +5923,10 @@ class WebAPIController extends Controller
         {
 
             info("info");
-            info($request->dist_id);
+            info($request['dist_id']);
             $result = SCTResult::where('is_deleted','no');
             if($request->dist_id) {
-                $result = $result->where('created_by', $request->dist_id);
+                $result = $result->where('created_by', $request['dist_id']);
             }
             $result = $result->orderBy('id', 'DESC')
                                 ->get();
