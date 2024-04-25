7fafeb316fbe5bf6a3cf9d1aba0da1e5c898f50c
Satish <satishaurange@sumagoinfotech.com>
2024-04-25 15:37:10 +0530

    changes for framer list filter by added by

--- app/Http/Controllers/FarmerController.php
+++ app/Http/Controllers/FarmerController.php
@@ -182,7 +182,7 @@ class FarmerController extends Controller
                 })
                 
                 ->when($request->get('added_by'), function($query) use ($request) {
-                    if($request->added_by =='super') {
+                    if($request->added_by =='superadmin') {
                         $query->where('usersinfo.added_by', $request->added_by);
                     } else {
                         $query->where('usersinfo.added_by', '<>', 'superadmin');
