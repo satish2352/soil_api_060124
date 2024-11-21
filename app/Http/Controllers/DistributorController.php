b94e1806882301251f1cd1fdf6ec2451cc6a79b6
Savita444 <savita.hajare@sumagoinfotech.com>
2024-11-21 18:28:51 +0530

    changes in the farmermeetinglist_distributorapp

--- app/Http/Controllers/DistributorController.php
+++ app/Http/Controllers/DistributorController.php
@@ -1947,11 +1947,14 @@ class DistributorController extends Controller
 {
     try {
         $response = [];
-        $farmerMeetingData = FarmerMeeting::select('tbl_farmer_meeting.*', 'tbl_farmer_meeting_details.farmer_id', 'tbl_farmer_meeting_details.farmer_fname', 'tbl_farmer_meeting_details.farmer_mname', 'tbl_farmer_meeting_details.farmer_lname')
+        $farmerMeetingData = FarmerMeeting::select('tbl_farmer_meeting.*', 'tbl_farmer_meeting_details.farmer_id', 'tbl_farmer_meeting_details.farmer_fname', 'tbl_farmer_meeting_details.farmer_mname', 'tbl_farmer_meeting_details.farmer_lname', 'stateNew.name as state_name',
+        'districtNew.name as district_name',
+        'talukaNew.name as taluka_name',
+        'cityNew.name as city_name')
             ->leftJoin('tbl_farmer_meeting_details', 'tbl_farmer_meeting.id', '=', 'tbl_farmer_meeting_details.farmer_meeting_table_id')
 
             ->leftJoin('tbl_area as stateNew', function ($join) {
-                $join->on('tbl_farmer_meeting .state', '=', 'stateNew.location_id');
+                $join->on('tbl_farmer_meeting.state', '=', 'stateNew.location_id');
             })
             ->leftJoin('tbl_area as districtNew', function ($join) {
                 $join->on('tbl_farmer_meeting.district', '=', 'districtNew.location_id');
@@ -1964,13 +1967,6 @@ class DistributorController extends Controller
             })
             ->where('tbl_farmer_meeting.is_deleted', 'no')
             ->where('tbl_farmer_meeting.created_by', $request->user_id)
-            ->select(
-                'tbl_farmer_meeting_details.*',
-                'stateNew.name as state_name',
-                'districtNew.name as district_name',
-                'talukaNew.name as taluka_name',
-                'cityNew.name as city_name'
-            )
             ->orderBy('tbl_farmer_meeting.id', 'DESC')
             ->get();
 
